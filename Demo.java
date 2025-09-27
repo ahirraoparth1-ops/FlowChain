bcf1183dea8f82597c4c91d63481e6373a84bdfe
ahirraoparth1-ops <ahirraoparth1@gmail.com>
2025-09-27 23:12:38 +0530

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,20 @@
+/*package com.company;*/
+
+public class Demo{
+
+    public static void main(String[] args) {
+
+        System.out.print("The sum of this no is this:");
+    
+    int num1 = 89;
+    int num2 = 80;
+    int num = 909;
+
+    int sum = num1+num2+num;
+
+    
+
+    System.out.println(sum);
+    }
+}
+
